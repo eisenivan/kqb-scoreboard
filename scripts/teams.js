d340dc66c5dc90f1551f5f1b52126232c38eb3c0
eisenivan <ivan.eisenberg@gmail.com>
2020-11-20 16:33:29 -0600

    add team and player stats to caching function

--- scripts/teams.js
+++ scripts/teams.js
@@ -37,31 +37,34 @@ if (!fs.existsSync(logoDir)) {
   fs.mkdirSync(logoDir)
 }
 
-const promises = circuits.map((circuit) => {
-  return axios.get(`https://indy-gaming-league-api.herokuapp.com/api/circuits/${circuit.id}/results?bucket=igl-teamlogopics`)
-    .then(data => {
-      data.data.data.forEach((team) => {
-        const key = md5(`${circuit.id}-${team.team.formattedName}`)
-        teams.push({
-          key,
-          name: team.team.formattedName,
-          selectName: `${team.team.formattedName} (${circuit.label})`,
-          logo: team.team.logo ? `/logos/${key}.jpg` : 'https://www.indygamingleague.com/static/media/igl_logo.d24d9c2f.png',
-          record: `${team.stats['Matches Won']} - ${team.stats['Matches Lost']}`
-        })
+circuits.map(async (circuit) => {
+  const data = await axios.get(`https://indy-gaming-league-api.herokuapp.com/api/circuits/${circuit.id}/results?bucket=igl-teamlogopics`)
 
-        if (team.team.logo) {
-          downloadImage(team.team.logo, `./public/logos/${key}.jpg`)
-        }
-      })
+  data.data.data.forEach(async (team) => {
+    console.log('Getting team:', team.team.formattedName)
+    const teamStats = await axios.get(`https://indy-gaming-league-api.herokuapp.com/api/circuits/${circuit.id}/stats?&bucket=igl-profilepics&location=PLAYER_STATS&id=${circuit.id}&team=${team.team._id}&statsType=player-circuit`)
+    const key = md5(`${circuit.id}-${team.team.formattedName}`)
+    teams.push({
+      key,
+      name: team.team.formattedName,
+      selectName: `${team.team.formattedName} (${circuit.label})`,
+      logo: team.team.logo ? `/logos/${key}.jpg` : 'https://www.indygamingleague.com/static/media/igl_logo.d24d9c2f.png',
+      record: `${team.stats['Matches Won']} - ${team.stats['Matches Lost']}`,
+      teamStats: _.get(team, 'stats', {}),
+      players: _.get(teamStats, 'data.data', []).map(player => ({
+        name: player.player.userName,
+        logo: player.player.logo,
+        stats: player.stats,
+        rankScore: player.rankScore
+      }))
     })
-    .catch(error => console.log('Request failed', error))
-})
 
-const today = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
+    if (team.team.logo) {
+      downloadImage(team.team.logo, `./public/logos/${key}.jpg`)
+    }
 
-Promise.all(promises)
-  .then(() => {
+    const today = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
+
     const sortedTeams = _.sortBy(teams, ['name'])
     fs.writeFileSync('./public/teams.json', JSON.stringify(sortedTeams, null, 2))
     fs.writeFileSync('./public/health.json', JSON.stringify({
@@ -72,3 +75,4 @@ Promise.all(promises)
     }, null, 2))
     console.log('Team JSON updated')
   })
+})
